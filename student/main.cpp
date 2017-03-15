27534e8163087250d409555d62ba30b1060995ef
Dawit Abera <dawitabera64@gmail.com>
2017-03-15 15:11:36 -0700

    Initial Commit

---
+++ student/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  student
+//
+//  Created by Dawit Abera on 3/15/17.
+//  Copyright © 2017 University of Washington. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
