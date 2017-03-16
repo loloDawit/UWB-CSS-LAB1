07164839e04c3173d0e06d8f02c5d99b75196041
Dawit Abera <dawitabera64@gmail.com>
2017-03-15 18:41:12 -0700

    the list sorted as we read the file.

--- student/student.cpp
+++ student/student.cpp
@@ -23,26 +23,40 @@ bool Student::sortInput(istream & infile, Student::StudentType * students, int &
             return true;
         }
         
-        int i = 0 ;
-        for (i=size; i > 0; i--){
-           
-            
-            if(strncmp(temp.last, students[i - 1].last, MAXLENGTH) < 0){
-                students[i] = students[i-1];
-            }
-            
-        
-        /* error check the grade. 
-           grade must be greater than 0 and less than 100 */
+        /* error check the grade.
+         grade must be greater than 0 and less than 100 */
         if(temp.grade < LOWGRADE || temp.grade > MAXGRADE){
             continue;                     // skip -ve values and >100 values
         }
         
-        // grades are fine, now check the names and sort it by last name
-        // if the last names are the same, check the first name to sort the list
-        // alphabetically
-       
+        int i = 0 ;
+        for (i=size; i > 0; i--){
+           
+            /* check the new student last name (temp.last) with the original
+               student last name from file.
+               strncmp() manipulation function is used to compare two strings
+               lexicographically until the characters differ or a null is encountered
+               or when specified count is reached. It returns an integer value.
+             */
+            if(strncmp(temp.last, students[i-1].last, MAXLENGTH) < 0){
+                students[i] = students[i-1];
+            }
+            
+            /* @this stage, the last names are the same, check the first name
+             to sort the data alphabetically.
+            */
+            else if (strncmp(temp.last, students[i-1].last, MAXLENGTH) == 0 &&
+                     strncmp(temp.first, students[i-1].first, MAXLENGTH) < 0){
+                students[i] = students[i-1];
+            }else{
+                // the list already sorted, break out
+                break;
+            }
+        }
+            // save the new student record to the array
+            students[i] = temp;
+            size++;
     }
-    
+        return true;
 
 }
